e0ba41005ff3eb8756a8d01d21ec25bda3f19d74
pras75299 <singh.prashantking@gmail.com>
2023-11-07 00:31:36 +0530

    add react-router-dom and create components basic structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
+import CreateBooks from './pages/CreateBooks'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import Home from './pages/Home'
+import ShowBook from './pages/ShowBook'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path='/' element={Home}/>
+        <Route path='/books/create' element={CreateBooks}/>
+        <Route path='/books/delete/:id' element={DeleteBook}/>
+        <Route path='/books/edit/:id' element={EditBook}/>
+        <Route path='/books/details/:id' element={ShowBook}/>
+      </Routes>
     </>
   )
 }
