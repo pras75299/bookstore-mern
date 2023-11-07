14988bf77c836e4d815e1489fd8cca5981a2a168
pras75299 <singh.prashantking@gmail.com>
2023-11-08 00:38:49 +0530

    add home component and backbutton common component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={Home}/>
-        <Route path='/books/create' element={CreateBooks}/>
-        <Route path='/books/delete/:id' element={DeleteBook}/>
-        <Route path='/books/edit/:id' element={EditBook}/>
-        <Route path='/books/details/:id' element={ShowBook}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/books/create' element={<CreateBooks/>}/>
+        <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+        <Route path='/books/edit/:id' element={<EditBook/>}/>
+        <Route path='/books/details/:id' element={<ShowBook/>}/>
       </Routes>
     </>
   )
